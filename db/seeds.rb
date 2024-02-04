e3941b62dffc8671dbc3e5ebf0bfa5ebc1919220
Louis Simon <louis.simon1994@gmail.com>
2024-02-04 16:08:17 +0100

    challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,38 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(
+  name: "Pizza Guy",
+  address: "Boxhagener Str 3, Berlin",
+  phone_number: "+49 123123123",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "La vie est belle",
+  address: "Alexanderplatz 1, Berlin",
+  phone_number: "+49345363",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Shiro",
+  address: "Warschauer Str. 3, Berlin",
+  phone_number: "+49456453",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Ming Dynastie",
+  address: "Janowitzbruecke",
+  phone_number: "+498529424",
+  category: "chinese"
+)
+
+Restaurant.create!(
+  name: "Waffle House",
+  address: "Charlottenburg, berlin",
+  phone_number: "+491231244",
+  category: "belgian"
+)
